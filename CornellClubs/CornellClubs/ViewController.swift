0662a215144bcb81ccd92dd34d3f87cc6302c417
Calli Sabaitis <css274@cornell.edu>
2019-11-19 22:13:04 -0500

    created project

---
+++ CornellClubs/CornellClubs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CornellClubs
+//
+//  Created by Calli Sabaitis on 11/19/19.
+//  Copyright © 2019 Calli Sabaitis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
